1f36bbe315c6231596ce874ec3b6d5515f99f7bc
wendy0107 <wendy920107@gmail.com>
2023-11-22 18:05:25 +0800

    update

--- camera/record_video.js
+++ camera/record_video.js
@@ -7,51 +7,47 @@ let FormData = require("form-data");
 
 const token = process.env.LINE_TOKEN;
 
-// Start to record a video...
-libcamera.video({
-        output: 'video/test_2.h264', // 注意：這是將影片保存為 H.264 格式
-        timeout: 5000, // 影片錄製時間（毫秒）
-        width: 640,
-        height: 480
-})
-        .then((result) => {
-                // Start to record a video and save.
-                console.log(result);
+const noti = () => {
+    let form_data = new FormData();
+    form_data.append("message", "Here's the video.");
+    form_data.append("imageFile", fs.createReadStream('video/test_2.h264'));
 
-                // Alert to Line Notify.
-                noti();
+    let headers = Object.assign({
+        'Authorization': Bearer ${token}
+    }, form_data.getHeaders());
+
+    axios({
+        method: 'post',
+        url: "https://notify-api.line.me/api/notify",
+        data: form_data,
+        headers: headers
+    })
+        .then((response) => {
+            console.log("HTTP StatusCode: " + response.status);
+            console.log(response.data);
         })
         .catch((error) => {
-                console.log(error);
+            console.error("Failed to send a line notification");
+            if (error.response) {
+                console.error("HTTP StatusCode: " + error.response.status);
+                console.error(error.response.data);
+            } else {
+                console.error(error);
+            }
         });
-
-const noti = () => {
-        let form_data = new FormData();
-        form_data.append("message", "Here's the video.");
-        form_data.append("imageFile", fs.createReadStream('video/test_2.h264')); // 注意：這是從 H.264 文件中讀取
-
-        let headers = Object.assign({
-                'Authorization': Bearer ${token} // 在此填入你的 Line Notify Token
-        }, form_data.getHeaders());
-
-        // 上傳影片到 Line Notify 透過 Line API
-        axios({
-                method: 'post',
-                url: "https://notify-api.line.me/api/notify",
-                data: form_data,
-                headers: headers
-        })
-                .then((response) => {
-                        console.log("HTTP StateCode:" + response.status);
-                        console.log(response.data);
-                })
-                .catch((error) => {
-                        console.error("Failed to send a line notification");
-                        if (error.response) {
-                                console.error("HTTP StatusCode:" + error.response.status);
-                                console.error(error.response.data);
-                        } else {
-                                console.error(error);
-                        }
-                });
 };
+
+// Start to record a video...
+libcamera.video({
+    output: 'video/test_2.h264',
+    timeout: 5000,
+    width: 640,
+    height: 480
+})
+    .then((result) => {
+        console.log(result);
+        noti(); // Call the noti function after it has been declared.
+    })
+    .catch((error) => {
+        console.log(error);
+    });
